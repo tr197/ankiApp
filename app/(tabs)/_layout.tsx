83c17595b9e4dbfd0020383454a2fa7b5c4cc801
thinkpc <huutri26697@gmail.com>
2024-02-06 15:03:38 +0700

    tabs style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,37 @@
-import { Tabs } from "expo-router";
+import { Link, Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 const Layout = () => {
-    return <Tabs />
-}
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.primary,
+        },
+        headerTintColor: "#fff",
+        tabBarActiveTintColor: Colors.primary,
+      }}
+    >
+      <Tabs.Screen
+        name="sets"
+        options={{
+          title: "My Sets",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Link href="/" asChild>
+              <TouchableOpacity style={{marginRight: 10}}>
+                <Ionicons name="add-outline" size={24} color={'#fff'}></Ionicons>
+              </TouchableOpacity>
+            </Link>
+          )
+        }}
+      ></Tabs.Screen>
+    </Tabs>
+  );
+};
 
-export default Layout
+export default Layout;
